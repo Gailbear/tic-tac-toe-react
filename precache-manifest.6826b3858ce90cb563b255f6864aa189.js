3d801a7df752bb7b7f71ad6e9df88144beebbd78
Gail Terman <gterman@gmail.com>
2018-12-14 10:51:20 -0500

    Updates

---
+++ precache-manifest.6826b3858ce90cb563b255f6864aa189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "4252e903400d7fa72fc6",
+    "url": "/tic-tac-toe/static/js/main.4252e903.chunk.js"
+  },
+  {
+    "revision": "fad7d4c437b8ad41b966",
+    "url": "/tic-tac-toe/static/js/1.fad7d4c4.chunk.js"
+  },
+  {
+    "revision": "4252e903400d7fa72fc6",
+    "url": "/tic-tac-toe/static/css/main.5629ea7f.chunk.css"
+  },
+  {
+    "revision": "0be66888f8bdf0e1a73c39100509db03",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
